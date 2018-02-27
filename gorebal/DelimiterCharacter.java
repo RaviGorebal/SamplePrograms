b7e695ca1e7b551ee28fe556fe01b8c4ad504a00
RAVIG-PC\Ravi <ravi.gorebaal@gmai.com>
2018-02-28 00:06:47 +0530

    3 rd commit

--- gorebal/DelimiterCharacter.java
+++ gorebal/DelimiterCharacter.java
@@ -6,27 +6,7 @@ import java.util.StringTokenizer;
 
 public class DelimiterCharacter {
 		
-		/*static public String[] split(String str, char delim)
-		  {
-		      if (str == null) {
-		          throw new NullPointerException("str can't be null");
-		      }
-
-		      // Note the javadoc on StringTokenizer:
-		      //     StringTokenizer is a legacy class that is retained for
-		      //     compatibility reasons although its use is discouraged in
-		      //     new code.
-		      // In other words, if StringTokenizer is ever removed from the JDK,
-		      // we need to have a look at String.split() (or java.util.regex)
-		      // if it is supported on a JSR169/Java ME platform by then.
-		      StringTokenizer st = new StringTokenizer(str, String.valueOf(delim));
-		      int n = st.countTokens();
-		      String[] s = new String[n];
-		      for (int i = 0; i < n; i++) {
-		          s[i] = st.nextToken();
-		      }
-		      return s;
-		  }*/
+		
 	public static void main(String args[])
 	  {		
 		ArrayList<Integer> al=new ArrayList<Integer>();
