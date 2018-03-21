9fa65d9f7c75548b1c9d29b50be8130788e9384d
RAVIG-PC\Ravi <ravi.gorebaal@gmai.com>
2018-03-21 23:05:08 +0530

    added prime number program

---
+++ gorebal/PrimeNumber.java
@@ -0,0 +1,26 @@
+package ravi.gorebal;
+
+public class PrimeNumber {
+	
+	public static void main(String args[]){
+		
+		String primenumber = "";
+		
+		for(int i=1;i<=100;i++){
+			
+			int counter =0;
+			
+			for(int num=i;num>=1;num--){				
+				if(i%num==0){				
+				counter = counter + 1;
+				}
+			}
+			
+			if(counter == 2){
+				primenumber = primenumber + i + " ";				
+			}			
+		}
+		System.out.println("primenumber between 1 t 100 : ");
+		System.out.println(primenumber);	
+	}
+}
