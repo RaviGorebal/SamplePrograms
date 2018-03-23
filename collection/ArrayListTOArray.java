b51f30126b465f2b4813dcd3fbdae362de9687ad
RAVIG-PC\Ravi <ravi.gorebaal@gmai.com>
2018-03-23 12:30:17 +0530

    hi

---
+++ collection/ArrayListTOArray.java
@@ -0,0 +1,29 @@
+package ravi.collection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//step1: convert from arrays to arraylist 
+//step2: adding the elements in arraylist
+//step: display the elements contains in arraylist
+
+
+public class ArrayListTOArray {
+	
+	public static void main(String args[]){
+		
+		String str[] = {"ravi","kumar"};
+		
+		List<String> list = new ArrayList<>(Arrays.asList(str));
+		
+		System.out.println(list);
+		
+		list.add("gorebal");
+		System.out.println(list);
+		for(String strloop:list){
+			System.out.println(strloop);
+			
+		}
+	}
+}
---
+++ gorebal/ReverseText.java
@@ -0,0 +1,31 @@
+
+package ravi.gorebal;
+
+import javax.xml.stream.events.Characters;
+
+public class ReverseText{
+	public static void main(String args[]){
+		 {
+		      String str = "world money";
+		      String[] split= str.split("");
+
+		      String revers = "";
+		      String revers1 = "";
+		      for (int i = 0; i<=split.length-1; i++)
+		    	  
+		      {		   
+//		    	  for(int j=i; j<=i;j--){
+//		    	  revers += split[j];
+//		    	  }
+		    	  System.out.print(split[i].length());
+		      }
+		      
+		      
+//		      System.out.println(revers);
+		    
+		     
+		   }  
+        
+      
+         }	
+}
