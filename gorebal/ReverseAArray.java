64cddda8283a5479c8f1b0ecbb22ab16f1cb29ab
RAVIG-PC\Ravi <ravi.gorebaal@gmai.com>
2018-03-23 22:23:44 +0530

    Reverse an Array

---
+++ gorebal/ReverseAArray.java
@@ -0,0 +1,23 @@
+package ravi.gorebal;
+
+import java.util.Arrays;
+
+public class ReverseAArray {
+
+	public static void main(String args[]){
+		
+	int ary[] = {101,102,103};
+	
+	int a[] =new int[ary.length];
+	int j= 0;
+	
+	for(int i=a.length-1;i>=0;i--){
+		
+		int g = (int) ary[i];
+		
+		a[j]=g;
+		j++;
+	}
+	System.out.println(Arrays.toString(a));	
+	}
+}
