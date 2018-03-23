a930efcd53d0ceee8fc0c97aa7d02ddb5b5c7113
RAVIG-PC\Ravi <ravi.gorebaal@gmai.com>
2018-03-23 12:14:20 +0530

    Program: Counting the number of elements in arraylist.

--- collection/CountValues.java
+++ collection/CountValues.java
@@ -1,5 +1,33 @@
 package ravi.collection;
 
-public class CountValues {
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
+public class CountValues {
+	
+	public static void main(String args[]){		
+		
+		List<Integer> list = new ArrayList<>();
+		
+		list.add(2);
+		list.add(2);
+		list.add(4);
+		list.add(6);
+		list.add(6);
+		System.out.println("List contains elements are : " + list);
+		
+		Set<Integer> set = new HashSet<>(list);
+		
+		for(int x:set){
+			int count=0;
+			for(int y:list){
+				if(x==y){
+					count++;					
+				}				
+			}			
+			System.out.println("Num is " + x + " collection is : " + count );
+		}		
+	}
 }
