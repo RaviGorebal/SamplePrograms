9f70a595c882c81a2f18d5b4e0970dddd4a17c28
Ravi Gorebal <30298865+RaviGorebal@users.noreply.github.com>
2018-03-31 01:48:27 +0530

    Update CapsChar.java

--- gorebal/CapsChar.java
+++ gorebal/CapsChar.java
@@ -6,9 +6,16 @@ public class CapsChar {
 	
 	public static void main(String args[]){
 	
-	String str = "ravi";
+// 	String str = "ravi";
 	
-	String str1 = (str.substring(0,1).toUpperCase()) + str.substring(1, str.length());
+// 	String str1 = (str.substring(0,1).toUpperCase()) + str.substring(1, str.length());
+		
+		String str = "ravi";
+	String str1 = str.substring(0,1).toUpperCase();
+	String str2 = str.substring(1, str.length());
+	String str3 = str1 + str2 ;
+	
+	System.out.println(str3);
 	
 	System.out.println(str1);
 	}
