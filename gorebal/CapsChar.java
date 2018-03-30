312f2f7648986a8f3e7d0c80983e91037761cf9a
RAVIG-PC\Ravi <ravi.gorebaal@gmai.com>
2018-03-31 02:21:34 +0530

    merging

--- gorebal/CapsChar.java
+++ gorebal/CapsChar.java
@@ -11,11 +11,28 @@ public class CapsChar {
 //	String str1 = (str.substring(0,1).toUpperCase()) + str.substring(1, str.length());
 
 	String str = "ravi";
-	String str1 = str.substring(0,1).toUpperCase();
-	String str2 = str.substring(1, str.length());
-	String str3 = str1 + str2 ;
+	
+	char ch[] = str.toCharArray();
+	
+	int length = ch.length;
+	
+	for(int i=0; i<=length;i++){
+		
+		String str1;
+		
+		if((i%2)==0){
+			 str1 = str.substring(i).toUpperCase();
+			
+		}else{
+			 str1 = str.substring(i).toLowerCase();
+		}
+		System.out.println(str1);
+	}
+//	String str1 = str.substring(0,1).toUpperCase();
+//	String str2 = str.substring(1, str.length());
+//	String str3 = str1 + str2 ;
 
-	System.out.println(str3);
+
 	}
 
 }
