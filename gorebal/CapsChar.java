1e6a623d5bc573170c330236e206f9ea2608c003
RAVIG-PC\Ravi <ravi.gorebaal@gmai.com>
2018-03-31 01:49:51 +0530

    commit from local

--- gorebal/CapsChar.java
+++ gorebal/CapsChar.java
@@ -6,12 +6,20 @@ public class CapsChar {
 	
 	public static void main(String args[]){
 	
+//	String str = "ravi";
+//	
+//	String str1 = (str.substring(0,1).toUpperCase()) + str.substring(1, str.length());
+	
 	String str = "ravi";
+	String str1 = str.substring(0,1).toUpperCase();
+	String str2 = str.substring(1, str.length());
+	String str3 = str1 + str2 ;
 	
-	String str1 = (str.substring(0,1).toUpperCase()) + str.substring(1, str.length());
-	
-	System.out.println(str1);
+	System.out.println(str3);
 	}
 
 }
 // tsting purpose
+
+
+
