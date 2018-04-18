cfbf6399ca05e933b41cde55b2ef600bd3b12632
Ravikumar Pampanagouda <ravikumar_pa@photonint.com>
2018-04-18 19:03:07 +0530

    Patterns programs

---
+++ gorebal/Pattern5.java
@@ -0,0 +1,19 @@
+package ravi.gorebal;
+
+public class Pattern5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=1;
+		for(int i=0; i<5; i++) {
+		
+			for(int j=0; j<=i; j++) {
+				System.out.print(num +" ");
+				num= num+1;
+			}
+			System.out.println();
+		}
+
+	}
+
+}
